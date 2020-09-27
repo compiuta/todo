73258ba326b5923867aa4c94664c4412805b4b82
compiuta <christianr@rocketmail.com>
2020-09-27 12:38:55 -0700

    add conditional AND operator to check if dataReturned has key items

--- app.js
+++ app.js
@@ -30,9 +30,9 @@
         },
         setData: function() {
             const dataReturned = model.getData();
-            const dataReturnedKeys = Object.keys(dataReturned) || 0;
 
-            if (dataReturnedKeys.length) {
+            if (dataReturned && (Object.keys(dataReturned) > 0)) {
+                const dataReturnedKeys = Object.keys(dataReturned)
                 const lastIdUsed = dataReturnedKeys[dataReturnedKeys.length - 1];
                 controller.data = dataReturned;
                 lastIdCreated = dataReturned[lastIdUsed].id;
